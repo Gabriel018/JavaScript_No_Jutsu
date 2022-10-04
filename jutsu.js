09434a4916f089fd64a776f26dfd2a7ce16e96e6
HenriqueVidal <hpvidal@outlook.com>
2022-10-03 23:36:17 -0300

    update com card active no clique

--- jutsu.js
+++ jutsu.js
@@ -1,29 +1,34 @@
 
-const btn = document.querySelectorAll(".card")
-const juju = document.querySelector(".jutsu")
-const modal = document.getElementById("mymodal")
+const grid = Array.from(document.querySelector(".grid")?.children);
+const btn = document.querySelectorAll(".card");
+const juju = document.querySelector(".jutsu");
+const modal = document.getElementById("mymodal");
 
 const clicados = []
 
-const fire = ['Dog','Galo','Porco']
+const fire = ['Dog', 'Galo', 'Porco']
 
 console.log(clicados)
-function mostrar(clicked){
-   
-   var audio = new Audio('img/itachi_Jutsu.mp3')
-   console.log(clicked)
- 
-   clicados.push(clicked) 
-   if(JSON.stringify(clicados)==JSON.stringify(fire)){ 
-    audio.play()  
-    modal.style.display="block"
-    setInterval(function(){
-    modal.style.display="none"  
-    },3000)
+const mostrar = (clicked, ev) => {
+
+   const audio = new Audio('img/itachi_Jutsu.mp3');
+
+   grid.forEach(card=> {
+      if (card.classList.value.includes("active")) {
+         card.classList.remove("active");
+      };
+   });
+   ev.classList.add("active");
+   console.log(clicked, ev)
+
+   clicados.push(clicked)
+   if (JSON.stringify(clicados) == JSON.stringify(fire)) {
+      audio.play()
+      modal.style.display = "block"
+      setInterval(function () {
+         modal.style.display = "none"
+      }, 3000)
    }
    console.log(clicados)
-   
-    }
 
-    
-   
+};
