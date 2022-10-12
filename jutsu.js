06e734813ded45fa13a51c1d2aa6708067da6893
Gabriel018 <duke.comandos@gmail.com>
2022-10-12 11:29:13 -0300

    selos

--- jutsu.js
+++ jutsu.js
@@ -5,17 +5,30 @@ const Btn = document.querySelector(".card");
 const juju = document.querySelector(".jutsu");
 const modal = document.getElementById("mymodal");
 
+const imgs = document.getElementById("img")
+const img  = document.querySelectorAll("#img img")
+
+let = dmx = 0
+
 const img_info = document.querySelector(".img_info")
 let modalActive = false; 
 const clicados = [];
 
 const fire = ['snake', 'dragon', 'rabbit','tiger']
 
+function carrosel(){
+   dmx++
+   if(dmx > img.length -1){
+   }
+   imgs.style.transform  = `translateX(${-dmx * 110}px)`
+}
+setInterval(carrosel,400)
+
+
 function mostrar_img(){
   img_info.style.display = "block"
 
 }
-
 function fechar_img(){
    img_info.style.display = "none"
 }
@@ -26,8 +39,8 @@ audio1.play()
 }
 console.log(clicados)
 const mostrar = async (clicked, ev) => {
+   carrosel()
    if (modalActive) return;
-
    const audio = new Audio('img/itachi_Jutsu.mp3');
 
    ev.classList.add("active");
