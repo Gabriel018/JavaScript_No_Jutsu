ad1e16f1a73ca30c41d0cc36c7530fe1f25e4401
Gabriel018 <duke.comandos@gmail.com>
2022-10-12 13:33:21 -0300

    time

--- jutsu.js
+++ jutsu.js
@@ -1,14 +1,15 @@
 
 const grid = Array.from(document.querySelector(".grid")?.children);
-const estilo_btn = document.querySelectorAll(".estilo")
-const Btn = document.querySelector(".card");
+const estilo_btn = document.querySelector(".estilo")
 const juju = document.querySelector(".jutsu");
 const modal = document.getElementById("mymodal");
-
+const carrosel_jujtsu = document.querySelector(".carrocel")
 const imgs = document.getElementById("img")
 const img  = document.querySelectorAll("#img img")
 
 let = dmx = 0
+
+let modal_jutus  = false
 
 const img_info = document.querySelector(".img_info")
 let modalActive = false; 
@@ -16,13 +17,15 @@ const clicados = [];
 
 const fire = ['snake', 'dragon', 'rabbit','tiger']
 
-function carrosel(){
+
+function carrosel_jutsu(){
    dmx++
    if(dmx > img.length -1){
+      dmx = 0
    }
    imgs.style.transform  = `translateX(${-dmx * 110}px)`
 }
-setInterval(carrosel,400)
+setInterval(carrosel_jutsu,300)
 
 
 function mostrar_img(){
@@ -33,27 +36,33 @@ function fechar_img(){
    img_info.style.display = "none"
 }
 
-function btn_estilo(){
-const audio1 = new Audio('img/Jutsu-Style.mp3')
-audio1.play()
-}
+
 console.log(clicados)
 const mostrar = async (clicked, ev) => {
-   carrosel()
    if (modalActive) return;
    const audio = new Audio('img/itachi_Jutsu.mp3');
-
    ev.classList.add("active");
    console.log(clicked)
-
    clicados.push(clicked)
    if (JSON.stringify(clicados) == JSON.stringify(fire)) {
+      carrosel_jujtsu.style.display = "block"
+      
+
+      setTimeout(function(){
+      
+      if( carrosel_jujtsu.style.display = "none"){
+         modalActive = true;
+         modal.style.display = "block";
+         audio.play()
+          
+      }   
+         
       modalActive = true;
       modal.style.display = "block";
-
-      clicados.length = 0;
-      await audio.play();
+      audio.play()
+       
       setTimeout(function () {
+         carrosel_jujtsu.style.display = "none"
          modal.style.display = "none";
          modalActive = false;
          grid.forEach(card => {
@@ -62,7 +71,11 @@ const mostrar = async (clicked, ev) => {
             };
          });
          console.log("time")
-      }, 3000);
+      }, 1000);
+      },1100)
+
+      clicados.length = 0;
+      
    };
    console.log(clicados);
 };
